50d8ef107befc25f47778b2ad2a7fedac6f166ad
t.f <thales.feres@hotmail.com>
2022-02-01 17:27:39 -0300

    new exercise

--- ola/ola.go
+++ ola/ola.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Olá, mundo")
+    fmt.Println("mundo")
 }
--- ola/ola_test.go
+++ ola/ola_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-    resultado := Ola()
-    esperado := "Olá, mundo"
+    resultado := Ola("Chris")
+    esperado := "Olá, Chris"
 
     if resultado != esperado {
         t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
